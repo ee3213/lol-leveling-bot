9b66c0e13e457e17269077281ac72cc69c743b93
Mitch <tplevesque2@gmail.com>
2022-05-09 12:32:50 -0300

    Updated locked camera again........

--- lol-leveling-bot/robot.py
+++ lol-leveling-bot/robot.py
@@ -220,7 +220,7 @@ def lock_screen():
         y = rect[1] + y
         win32api.SetCursorPos((x, y))
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-        time.sleep(0.1)
+        time.sleep(0.5)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
     except Exception:
         pass
